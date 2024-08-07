5088e8993e007932417eb85018d30df3f846878e
cmsshg <hg6332955089@163.com>
2024-08-07 16:23:44 +0800

    Update range.go

--- pkg/filter/range.go
+++ pkg/filter/range.go
@@ -11,20 +11,29 @@ type Range struct {
 
 type RangeList struct {
 	list []*Range
+	minLeft  uint16
+	maxRight uint16
 }
 
 func NewRangeList() *RangeList {
 	return &RangeList{
 		list: make([]*Range, 0),
+		minLeft:  0,
+		maxRight: 0,
 	}
 }
 
+type ByLeft []*Range
+
 func (rl *RangeList) IsSlotInList(key string) bool {
+	keySlot := redis.KeyToSlot(key)
 	if len(rl.list) == 0 {
 		return false
 	}
-	keySlot := redis.KeyToSlot(key)
-	
+	if keySlot < rl.minLeft || keySlot > rl.maxRight {
+		return false
+	}
+
 	left, right := 0, len(rl.list)-1
 	for left <= right {
 		mid := left + (right-left)/2
@@ -49,5 +58,11 @@ func (rl *RangeList) InsertSlotInList(left, right uint16) {
 		rl.list = append(rl.list, nil)
 		copy(rl.list[i+1:], rl.list[i:])
 		rl.list[i] = newRange
+		if left < rl.minLeft {
+			rl.minLeft = left
+		}
+		if right > rl.maxRight {
+			rl.maxRight = right
+		}
 	}
 }
